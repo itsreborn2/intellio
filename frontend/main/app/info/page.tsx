55628b6db3f1f29f4b5b65d0fdd6e4b29c5b3bc3
kyuni392 <PLANA392@GMAIL.COM>
2025-06-02 00:15:04 +0900

    홈피 수정중중

--- frontend/main/app/info/page.tsx
+++ frontend/main/app/info/page.tsx
@@ -5,7 +5,7 @@ export default function InfoPage() {
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-4" style={{ color: 'oklch(0.372 0.044 257.287)' }}>Info StockEasy</h1>
       <p className="text-lg" style={{ color: 'oklch(0.372 0.044 257.287)' }}>
-      <iframe src="https://zealous-spice-207.notion.site/ebd/1d6a7c3f6a648059820ce737d48c65f9" width="100%" height="600" frameborder="0" allowfullscreen />
+      <iframe src="https://zealous-spice-207.notion.site/ebd/1d6a7c3f6a648059820ce737d48c65f9" width="100%" height="600" frameBorder="0" allowFullScreen />
       </p>
     </div>
   );
